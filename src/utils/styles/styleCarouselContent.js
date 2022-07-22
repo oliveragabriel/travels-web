64e29e4b925778578298a8ab83b0c72dba301cb2
oliveragabriel <g.olivera1993@gmail.com>
2022-07-21 21:28:06 -0300

    feat: create common styles folder on utils

---
+++ src/utils/styles/styleCarouselContent.js
@@ -0,0 +1,7 @@
+export const styleCarouselContent = {
+  height: '325px',
+  color: '#fff',
+  lineHeight: '325px',
+  textAlign: 'center',
+  background: '#364d79',
+};
---
+++ src/utils/styles/styleIcon.js
@@ -0,0 +1,22 @@
+export const styleIconSizeTwenty = {
+  fontSize: 20,
+};
+
+export const styleIconSizeTwentyAndColor = {
+  fontSize: 20, 
+  color: '#FF8C00'
+};
+
+export const styleIconSizeThirtyAndGhost = {
+  fontSize: 30, 
+  color: 'whitesmoke'
+};
+
+export const styleIconSizeThirty = {
+  fontSize: 30,
+};
+  
+export const styleIconSizeThirtyAndColor = {
+  fontSize: 30, 
+  color: '#FF8C00'
+};
